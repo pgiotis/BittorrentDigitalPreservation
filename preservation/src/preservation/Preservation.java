4948b6501e14950e24705ed2395dd882b4925b48
panagiotis <panagiotis@panagiotis-laptop.(none)>
2013-04-26 15:01:29 +0300

    The first version.

---
+++ preservation/src/preservation/Preservation.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package preservation;
+
+/**
+ *
+ * @author panagiotis
+ */
+public class Preservation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("preservation starts");
+        //connect to DB tracker
+        
+        //query= select info_hash,filename,seeders,strategy,startdate from namemap
+        
+        //for each record
+            
+            //if (nowDate -startdate)>5
+                //query= select count(*) from x+info_hash where status='seeders'
+                //if query< threshold
+                    //execute strategy
+        
+        
+        
+    }
+}
