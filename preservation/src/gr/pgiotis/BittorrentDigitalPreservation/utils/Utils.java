e99fa940b4c963e5170e78ab52dacef44aaeae18
pgiotis <pgiotis@scify.org>
2013-04-26 23:05:09 +0300

    Create logFileCreator method

--- preservation/src/gr/pgiotis/BittorrentDigitalPreservation/utils/Utils.java
+++ preservation/src/gr/pgiotis/BittorrentDigitalPreservation/utils/Utils.java
@@ -4,16 +4,52 @@
  */
 package gr.pgiotis.BittorrentDigitalPreservation.utils;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
  * 
  * @author Panagiotis Giotis <giotis.p@gmail.com>
  */
 public class Utils {
     
-    //write to file
-    
-    //
-    
+     /**
+     * Create the log files
+     *
+     * @param text the text that i want to write in the file
+     * @param appendToFile boolean parameter for the append
+     */
+    public static void createLogFile(String text,String logName ,boolean appendToFile) {
+
+        PrintWriter pw = null;
+
+        try {
+
+            if (appendToFile) {
+
+                pw = new PrintWriter(new FileWriter("./logFiles/"+logName+".log", true));  //If the file already exists, start writing at the end of it.
+
+            } else {
+
+                pw = new PrintWriter(new FileWriter("./logFiles/"+logName+".log"));
+
+            }
+
+            pw.println(text);                                          // write to output.out
+            pw.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            //Close the PrintWriter
+            if (pw != null) {
+                pw.close();
+            }
+
+        }
+
+    }
     
     
     
