ac6595f618772b714d15a4d11ecd108c164f33ff
pgiotis <pgiotis@scify.org>
2013-04-27 21:05:38 +0300

    add   - SchedulePreservetion.sh clean-build   -JDK6

---
+++ preservation/SchedulePreservetion.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+java -cp dist/preservation.jar:dist/lib/mail:dist/lib/LibSVM.jar:dist/lib/mysql-connector-java-5.1.24-bin.jar gr.pgiotis.BittorrentDigitalPreservation.preservation.Preservation
+at -f ./SchedulePreservetion.sh now + 1 minute
+ 
